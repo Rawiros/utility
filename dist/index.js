c43f2b281fc88b444ab533c2d5482494ae3135a6
Rawiros <rawir@disroot.org>
2024-03-20 09:55:39 +0100

    update

--- dist/index.js
+++ dist/index.js
@@ -32,104 +32,106 @@ exports.Queue = Queue_1.default;
 const YAMLConfig_1 = __importDefault(require("./YAMLConfig"));
 exports.YAMLConfig = YAMLConfig_1.default;
 icons_json_1.icons.Placeholder = icons_json_1.icons[icons_json_1.placeholder];
-// @ts-ignore
-if (!globalThis?.DOT) {
-    const EMPTY = "᲼";
-    exports.EMPTY = EMPTY;
-    const DOT = "•";
-    exports.DOT = DOT;
-    Object.defineProperties(globalThis, {
-        EMPTY: { get() { return EMPTY; } },
-        DOT: { get() { return DOT; } }
-    });
+;
+if (!Math['toByte']) {
+    // @ts-ignore
+    if (!globalThis?.DOT) {
+        const EMPTY = "᲼";
+        exports.EMPTY = EMPTY;
+        const DOT = "•";
+        exports.DOT = DOT;
+        Object.defineProperties(globalThis, {
+            EMPTY: { get() { return EMPTY; } },
+            DOT: { get() { return DOT; } }
+        });
+    }
+    Math.toByte = degrees => {
+        let b = Math.floor((degrees % 360) * 256 / 360);
+        if (b < -128)
+            b += 256;
+        else if (b > 127)
+            b -= 256;
+        return b;
+    };
+    Math.limit = (value, min = 0, max = 128) => Math.min(Math.max(value, min), max);
+    Array.prototype.random = function () {
+        return this[Math.floor(Math.random() * this.length)];
+    };
+    String.prototype.equals = function (value) {
+        return this === value;
+    };
+    String.prototype.format = function (data = {}) {
+        let text = String(this);
+        for (const key of Object.keys(data))
+            text = text.replaceAll(`{${key}}`, data[key]);
+        return text;
+    };
+    String.prototype.limit = function (length = 1970) {
+        if (this.length >= length)
+            return this.slice(0, length - 3).concat("...");
+        return this;
+    };
+    Boolean.prototype.is = function (value) {
+        return this.valueOf() === value;
+    };
+    // Numbers
+    const rule = /\.0+$|(.\d*[1-9])0+$/;
+    const lookup = [
+        { value: 1, symbol: "" },
+        { value: 1e3, symbol: "k" },
+        { value: 1e6, symbol: "M" },
+        { value: 1e9, symbol: "G" },
+        { value: 1e12, symbol: "T" },
+        { value: 1e15, symbol: "P" },
+        { value: 1e18, symbol: "E" }
+    ];
+    Number.prototype.format = function (digits = 2) {
+        const value = this.valueOf();
+        const item = lookup.slice().reverse().find(item => value >= item.value);
+        return item ? (value / item.value).toFixed(digits).replace(rule, "$1") + item.symbol : "0";
+    };
+    Number.prototype.equals = function (value) {
+        return this.valueOf() === value;
+    };
+    Number.prototype.formatBytes = function (decimals = undefined) {
+        return (0, formatBytes_1.default)(this.valueOf(), decimals);
+    };
+    Number.prototype.addFlag = function (flag) {
+        return this.valueOf() | (1 << flag);
+    };
+    Number.prototype.hasFlag = function (flag) {
+        return (this.valueOf() & (1 << flag)) !== 0;
+    };
+    Number.prototype.removeFlag = function (flag) {
+        return Math.max(this.valueOf() & ~(1 << flag), 0);
+    };
+    Number.prototype.limit = function (min = 0, max = 128) {
+        return Math.min(Math.max(this.valueOf(), min), max);
+    };
+    // BigInts
+    BigInt.prototype.addFlag = function (flag) {
+        return this.valueOf() | (1n << flag);
+    };
+    BigInt.prototype.hasFlag = function (flag) {
+        return (this.valueOf() & (1n << flag)) !== 0n;
+    };
+    BigInt.prototype.removeFlag = function (flag) {
+        return this.valueOf() < 0n ? 0n : this.valueOf() & ~(1n << flag);
+    };
+    ;
+    BigInt.prototype.is = function (value) {
+        return this === value.valueOf();
+    };
+    BigInt.prototype.toNumber = function () {
+        return Number(this.valueOf());
+    };
+    Boolean.prototype.toIcon = function () {
+        return this ? icons_json_1.icons.Yes : icons_json_1.icons.No;
+    };
+    String.prototype.firstToUpperCase = function () {
+        return this.split("_").map(e => e[0].toUpperCase() + e.slice(1).toLowerCase()).join("");
+    };
 }
-;
-Math.toByte = degrees => {
-    let b = Math.floor((degrees % 360) * 256 / 360);
-    if (b < -128)
-        b += 256;
-    else if (b > 127)
-        b -= 256;
-    return b;
-};
-Math.limit = (value, min = 0, max = 128) => Math.min(Math.max(value, min), max);
-Array.prototype.random = function () {
-    return this[Math.floor(Math.random() * this.length)];
-};
-String.prototype.equals = function (value) {
-    return this === value;
-};
-String.prototype.format = function (data = {}) {
-    let text = String(this);
-    for (const key of Object.keys(data))
-        text = text.replaceAll(`{${key}}`, data[key]);
-    return text;
-};
-String.prototype.limit = function (length = 1970) {
-    if (this.length >= length)
-        return this.slice(0, length - 3).concat("...");
-    return this;
-};
-Boolean.prototype.is = function (value) {
-    return this.valueOf() === value;
-};
-// Numbers
-const rule = /\.0+$|(.\d*[1-9])0+$/;
-const lookup = [
-    { value: 1, symbol: "" },
-    { value: 1e3, symbol: "k" },
-    { value: 1e6, symbol: "M" },
-    { value: 1e9, symbol: "G" },
-    { value: 1e12, symbol: "T" },
-    { value: 1e15, symbol: "P" },
-    { value: 1e18, symbol: "E" }
-];
-Number.prototype.format = function (digits = 2) {
-    const value = this.valueOf();
-    const item = lookup.slice().reverse().find(item => value >= item.value);
-    return item ? (value / item.value).toFixed(digits).replace(rule, "$1") + item.symbol : "0";
-};
-Number.prototype.equals = function (value) {
-    return this.valueOf() === value;
-};
-Number.prototype.formatBytes = function (decimals = undefined) {
-    return (0, formatBytes_1.default)(this.valueOf(), decimals);
-};
-Number.prototype.addFlag = function (flag) {
-    return this.valueOf() | (1 << flag);
-};
-Number.prototype.hasFlag = function (flag) {
-    return (this.valueOf() & (1 << flag)) !== 0;
-};
-Number.prototype.removeFlag = function (flag) {
-    return Math.max(this.valueOf() & ~(1 << flag), 0);
-};
-Number.prototype.limit = function (min = 0, max = 128) {
-    return Math.min(Math.max(this.valueOf(), min), max);
-};
-// BigInts
-BigInt.prototype.addFlag = function (flag) {
-    return this.valueOf() | (1n << flag);
-};
-BigInt.prototype.hasFlag = function (flag) {
-    return (this.valueOf() & (1n << flag)) !== 0n;
-};
-BigInt.prototype.removeFlag = function (flag) {
-    return this.valueOf() < 0n ? 0n : this.valueOf() & ~(1n << flag);
-};
-;
-BigInt.prototype.is = function (value) {
-    return this === value.valueOf();
-};
-BigInt.prototype.toNumber = function () {
-    return Number(this.valueOf());
-};
-Boolean.prototype.toIcon = function () {
-    return this ? icons_json_1.icons.Yes : icons_json_1.icons.No;
-};
-String.prototype.firstToUpperCase = function () {
-    return this.split("_").map(e => e[0].toUpperCase() + e.slice(1).toLowerCase()).join("");
-};
 const Icon = (name, text) => {
     if (!text)
         return icons_json_1.icons[name];
--- src/index.ts
+++ src/index.ts
@@ -15,18 +15,6 @@ import YAMLConfig from './YAMLConfig';
 
 _icons.Placeholder = _icons[placeholder as keyof typeof _icons];
 
-// @ts-ignore
-if (!globalThis?.DOT) {
-    const EMPTY = "᲼";
-    const DOT = "•";
-
-    Object.defineProperties(globalThis, {
-        EMPTY: { get() { return EMPTY } },
-        DOT: { get() { return DOT } }
-    });
-
-}
-
 declare global {
     const DOT: string
     const EMPTY: string
@@ -67,114 +55,128 @@ declare global {
     }
 };
 
-Math.toByte = degrees => {
-    let b = Math.floor((degrees % 360) * 256 / 360)
-    if (b < -128) b += 256
-    else if (b > 127) b -= 256
-    return b;
-};
+if (!Math['toByte']) {
+    // @ts-ignore
+    if (!globalThis?.DOT) {
+        const EMPTY = "᲼";
+        const DOT = "•";
 
-Math.limit = (value: number, min = 0, max = 128) => Math.min(Math.max(value, min), max);
+        Object.defineProperties(globalThis, {
+            EMPTY: { get() { return EMPTY } },
+            DOT: { get() { return DOT } }
+        });
+    }
 
-Array.prototype.random = function () {
-    return this[Math.floor(Math.random() * this.length)];
+    Math.toByte = degrees => {
+        let b = Math.floor((degrees % 360) * 256 / 360)
+        if (b < -128) b += 256
+        else if (b > 127) b -= 256
+        return b;
+    };
+
+    Math.limit = (value: number, min = 0, max = 128) => Math.min(Math.max(value, min), max);
+
+    Array.prototype.random = function () {
+        return this[Math.floor(Math.random() * this.length)];
+    }
+
+    String.prototype.equals = function (value) {
+        return this === value;
+    };
+
+    String.prototype.format = function (data = {}) {
+        let text = String(this);
+
+        for (const key of Object.keys(data))
+            text = text.replaceAll(`{${key}}`, data[key]);
+
+        return text;
+    };
+
+    String.prototype.limit = function (length = 1970) {
+        if (this.length >= length)
+            return this.slice(0, length - 3).concat("...")
+
+        return this as string;
+    };
+
+    Boolean.prototype.is = function (value) {
+        return this.valueOf() === value;
+    };
+
+    // Numbers
+    const rule = /\.0+$|(.\d*[1-9])0+$/;
+    const lookup = [
+        { value: 1, symbol: "" },
+        { value: 1e3, symbol: "k" },
+        { value: 1e6, symbol: "M" },
+        { value: 1e9, symbol: "G" },
+        { value: 1e12, symbol: "T" },
+        { value: 1e15, symbol: "P" },
+        { value: 1e18, symbol: "E" }
+    ];
+
+    Number.prototype.format = function (digits = 2) {
+        const value = this.valueOf();
+        const item = lookup.slice().reverse().find(item => value >= item.value);
+
+        return item ? (value / item.value).toFixed(digits).replace(rule, "$1") + item.symbol : "0";
+    }
+
+    Number.prototype.equals = function (value) {
+        return this.valueOf() === value;
+    };
+
+    Number.prototype.formatBytes = function (decimals = undefined) {
+        return formatBytes(this.valueOf(), decimals)
+    }
+
+    Number.prototype.addFlag = function (flag) {
+        return this.valueOf() | (1 << flag);
+    };
+
+    Number.prototype.hasFlag = function (flag) {
+        return (this.valueOf() & (1 << flag)) !== 0;
+    };
+
+    Number.prototype.removeFlag = function (flag) {
+        return Math.max(this.valueOf() & ~(1 << flag), 0)
+    };
+
+    Number.prototype.limit = function (min = 0, max = 128) {
+        return Math.min(Math.max(this.valueOf(), min), max)
+    };
+
+    // BigInts
+    BigInt.prototype.addFlag = function (flag) {
+        return this.valueOf() | (1n << flag);
+    };
+
+    BigInt.prototype.hasFlag = function (flag) {
+        return (this.valueOf() & (1n << flag)) !== 0n;
+    };
+
+    BigInt.prototype.removeFlag = function (flag) {
+        return this.valueOf() < 0n ? 0n : this.valueOf() & ~(1n << flag);
+    };;
+
+    BigInt.prototype.is = function (value) {
+        return this === value.valueOf();
+    };
+
+    BigInt.prototype.toNumber = function () {
+        return Number(this.valueOf());
+    };
+
+    Boolean.prototype.toIcon = function () {
+        return this ? _icons.Yes : _icons.No;
+    };
+
+    String.prototype.firstToUpperCase = function () {
+        return (this as string).split("_").map(e => e[0].toUpperCase() + e.slice(1).toLowerCase()).join("");
+    };
+
 }
-
-String.prototype.equals = function (value) {
-    return this === value;
-};
-
-String.prototype.format = function (data = {}) {
-    let text = String(this);
-
-    for (const key of Object.keys(data))
-        text = text.replaceAll(`{${key}}`, data[key]);
-
-    return text;
-};
-
-String.prototype.limit = function (length = 1970) {
-    if (this.length >= length)
-        return this.slice(0, length - 3).concat("...")
-
-    return this as string;
-};
-
-Boolean.prototype.is = function (value) {
-    return this.valueOf() === value;
-};
-
-// Numbers
-const rule = /\.0+$|(.\d*[1-9])0+$/;
-const lookup = [
-    { value: 1, symbol: "" },
-    { value: 1e3, symbol: "k" },
-    { value: 1e6, symbol: "M" },
-    { value: 1e9, symbol: "G" },
-    { value: 1e12, symbol: "T" },
-    { value: 1e15, symbol: "P" },
-    { value: 1e18, symbol: "E" }
-];
-
-Number.prototype.format = function (digits = 2) {
-    const value = this.valueOf();
-    const item = lookup.slice().reverse().find(item => value >= item.value);
-
-    return item ? (value / item.value).toFixed(digits).replace(rule, "$1") + item.symbol : "0";
-}
-
-Number.prototype.equals = function (value) {
-    return this.valueOf() === value;
-};
-
-Number.prototype.formatBytes = function (decimals = undefined) {
-    return formatBytes(this.valueOf(), decimals)
-}
-
-Number.prototype.addFlag = function (flag) {
-    return this.valueOf() | (1 << flag);
-};
-
-Number.prototype.hasFlag = function (flag) {
-    return (this.valueOf() & (1 << flag)) !== 0;
-};
-
-Number.prototype.removeFlag = function (flag) {
-    return Math.max(this.valueOf() & ~(1 << flag), 0)
-};
-
-Number.prototype.limit = function (min = 0, max = 128) {
-    return Math.min(Math.max(this.valueOf(), min), max)
-};
-
-// BigInts
-BigInt.prototype.addFlag = function (flag) {
-    return this.valueOf() | (1n << flag);
-};
-
-BigInt.prototype.hasFlag = function (flag) {
-    return (this.valueOf() & (1n << flag)) !== 0n;
-};
-
-BigInt.prototype.removeFlag = function (flag) {
-    return this.valueOf() < 0n ? 0n : this.valueOf() & ~(1n << flag);
-};;
-
-BigInt.prototype.is = function (value) {
-    return this === value.valueOf();
-};
-
-BigInt.prototype.toNumber = function () {
-    return Number(this.valueOf());
-};
-
-Boolean.prototype.toIcon = function () {
-    return this ? _icons.Yes : _icons.No;
-};
-
-String.prototype.firstToUpperCase = function () {
-    return (this as string).split("_").map(e => e[0].toUpperCase() + e.slice(1).toLowerCase()).join("");
-};
 
 const Icon = <IconName extends keyof typeof _icons>(name: IconName, text?: string) => {
     if (!text)
